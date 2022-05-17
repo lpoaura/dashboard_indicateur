b07197acb584184cbcf66cc855772f2b8d099160
BULTINGAIRE <bultingairet@gmail.com>
2022-05-17 18:25:58 +0200

    Première version de l'application (sans js) après la 1ere journée de travail. Il manque du dynamisme à réaliser avec des tabPanel.

---
+++ R/createEnTete.R
@@ -0,0 +1,84 @@
+# Création de l'en-tête
+
+
+# Logo de la LPO
+divLogo <- div(id = "logoLPO",
+               img(src = "logo_lpo.png",
+                   width = 150,
+                   height = 150))
+
+
+# Logo du laboratoire
+divLogo1 <- div(id = "logo1",
+                img(src = "Logo-observatoire.png",
+                    width = 70,
+                    height = 70))
+
+# Autres logos
+# divSousLogos <- div(id = "autresLogos",
+#                     img(src = "logo_region.png",
+#                         width = 40,
+#                         height = 40),
+#                     img(src = "logo_ofb.png",
+#                         width = 40,
+#                         height = 40),
+#                     img(src = "logo_Flavia.png",
+#                         width = 40,
+#                         height = 40),
+#                     img(src = "logo_cbn.png",
+#                         width = 40,
+#                         height = 40))
+
+# divTitreSousLogos <- div(id = "titreSousLogos",
+#                          header,
+#                          divSousLogos)
+
+# Titre du Dashboard
+header <- titlePanel(title = "TITRE DU DASHBOARD",
+                     windowTitle = "TITRE WINDOW"
+)
+
+divLogoTitre <- div(id = "logoTitre",
+                    divLogo1,
+                    header)
+rm(divLogo1, header)
+
+
+# Boutons de navigation
+ulNavigation <- tags$ul(id = "navigation",
+                        actionButton(inputId = "accueilBouton",
+                                     label = "ACCUEIL"),
+                        actionButton(inputId = "globalBouton",
+                                     label = "GLOBAL"),
+                        actionButton(inputId = "expertBouton",
+                                     label = "EXPERT"),
+                        actionButton(inputId = "adminBouton",
+                                     label = "ADMIN"))
+
+divLogoHeader <- div(id = "logoHeader",
+                     divLogoTitre,
+                     ulNavigation)
+rm(divLogoTitre, ulNavigation)
+
+
+# Bandeau de settings
+divSettings <- div(id = "settings",
+                   "SETTINGS")
+
+# Bandeau vide
+divBlankHeader <- div(id = "blankHeader")
+
+
+# Partie droite du bandeau
+divHeadBand <- div(id = "headBand",
+                   divLogoHeader,
+                   divSettings,
+                   divBlankHeader)
+rm(divLogoHeader, divSettings, divBlankHeader)
+
+
+# En tête générale
+divEnTete <- div(id = "enTete",
+                 divLogo,
+                 divHeadBand)
+
--- app.R
+++ app.R
@@ -1,62 +1,131 @@
-# . -------------------------------------------------------------------------- =============
-# 0 - Objectifs ====
-# . -------------------------------------------------------------------------- =============
-
-# Le script a pour objectif de realiser des requetes sur la BDD
-
-# . -------------------------------------------------------------------------- =============
-# 1 - Librairie ====
-# . -------------------------------------------------------------------------- =============
-
-## differentes librairies utiles
-# gestion de connexion BDD : "RPostgreSQL", "DBI"
-# manipulation de données : "tidyverse","lubridate","stringr"
-# visualisation de données : "ggplot2","RColorBrewer", "plotly"
-# manipulation de données géographique : "sf"
+# app.R
 
 pkgs <-  c("RPostgreSQL", "DBI","dplyr","tidyverse","lubridate","stringr", "ggplot2","RColorBrewer", "plotly","sf")
 
 # on test si les librairies sont installees sur l'environnement, si c'est pas le cas on les installes
 if (length(setdiff(pkgs, rownames(installed.packages()))) > 0) {
-  # installation des packages 
-  install.packages(setdiff(pkgs, rownames(installed.packages())))  
-} 
-# chargement des librairies (necessite qu'elles soient installees) 
+  # installation des packages
+  install.packages(setdiff(pkgs, rownames(installed.packages())))
+}
+# chargement des librairies (necessite qu'elles soient installees)
 lapply(pkgs, library, character.only = TRUE)
 rm(pkgs)
 
-# . -------------------------------------------------------------------------- =============
-# 2 - connexion à la BDD ====
-# . -------------------------------------------------------------------------- =============
 
-## Connexion a la base de donnee LPO AURA
-getwd() %>% ## prise du chemin de dossier actuelle 
-  paste0("/R/connexion_lpo.R") %>% ## indication du nom du fichier 
-  source() ## lecture du fichier 
-
-## autres options similaires : 
-# source("C:/Users/afourneyron/Documents/dashboard_indicateur/connexion_lpo.R")
-
-
-# . -------------------------------------------------------------------------- =============
-# 3 - lectures des donnees par requete ====
-# . -------------------------------------------------------------------------- =============
-
-## TAB_Info va etre la variable qui va recuperer les donnees 
-## dbGetQuery c'est la fonction qui permet de recuperer les donnees
-## con_gn c'est la variable de connexion a la base de donnees (defini dans le script connexion_lpo.R)
-## Puis apres c'est simplement votre requetes SQL
-TAB_Info <- dbGetQuery(con_gn, "select * from orb_indicateurs.mv_sraddet_ind_pole") 
+library(shiny)
+library(shinydashboard)
 
 
 
+getwd() %>% ## prise du chemin de dossier actuelle
+  paste0("/R/createEnTete.R") %>%
+  source() 
 
-# . -------------------------------------------------------------------------- =============
-# 4 - lectures des donnees spatiales par requete ====
-# . -------------------------------------------------------------------------- =============
+getwd() %>% ## prise du chemin de dossier actuelle
+  paste0("/R/createLeftBandeau.R") %>%
+  source() 
 
-## si on veux recuperer des entites geometriques il est necessaires de passer par le package "sf" et la fonction st_read
-## elle marche de la meme manieres mais la variable ne seras pas un tableau mais un tableau d'entites spatiales.
-GEOM_Info <- st_read(con_gn, query = "select * from orb_indicateurs.mv_sraddet_ind_pole_geom" ) 
 
-# plot(GEOM_Info$geom[1])
+# Carte en background
+divCarteBack <- div(id = "carteBack",
+                   "TEST CARTE")
+
+# Page entière
+divTotal <- div(id = "total",
+               divCarteBack,
+               divEnTete,
+               divBandeauCarte)
+
+# Page entière
+divTotalAccueil <- div(id = "total",
+                       divCarteBack,
+                       divEnTeteAccueil,
+                       divBandeauCarte)
+
+ui <- fluidPage(
+  
+  includeCSS("www/enTeteCSS.css"),
+  includeCSS("www/leftBandeauCSS.css"),
+  includeCSS("www/generalCSS.css"),
+  
+  # numericInput(inputId = "compteur", label =  "compteur", value = 2),
+  
+  # if (input$compteur == 1) {
+  #   divTotalAccueil
+  # } else if (input$compteur == 2) {
+  #   divTotal
+  # }
+  divTotal
+  
+)
+
+
+server <- function(input, output, session) {
+  
+  # counter <- reactiveValues(countervalue = 2)
+  # 
+  # observeEvent(input$accueilBouton, {
+  #   counter$countervalue <- 1
+  #   updateNumericInput(session = session, inputId = "compteur", label = "compteur", value = 1)
+  # })
+  # 
+  # observeEvent(input$globalBouton, {
+  #   counter$countervalue <- 2
+  #   updateNumericInput(session = session, inputId = "compteur", label = "compteur", value = 2)
+  # })
+}
+
+
+shinyApp(ui = ui, server = server)
+
+# rm(list = ls())
+
+
+# numericInput(inputId = "n", 
+#              "Sample size", value = 25),
+# plotOutput(outputId = "hist"),
+# checkboxInput(inputId = "b",
+#               label = tags$img(src = "patte.png",
+#                                width = "30",
+#                                height = "30")),
+# checkboxInput(inputId = "b",
+#               label = tags$a(href="https://trello.com/b/0u889JNF/ping",
+#                              img(src = "patte.png",
+#                                  width = "30",
+#                                  height = "30"))),
+# actionButton("button",
+#              tags$a(href="https://trello.com/b/0u889JNF/ping",
+#                     img(src = "patte.png",
+#                         width = "30",
+#                         height = "30"))),
+# tags$button(id = "actionButt",
+#             class = "btn action-button",
+#             tags$img(src = "patte.png"),
+#             height = "50px",
+#             width = "50px"),
+# actionButton(inputId = "test",
+#              label = img(src = "patte.png",
+#                          width = "30",
+#                          height = "30")),
+# actionButton(inputId = "testClassic",
+#              label = NULL,
+#              style = "width: 50px; height: 50px;
+#              backgroung-image: url('/www/patte.png');
+#              background-size: cover;
+#              background-position: center;"),
+# actionLink(inputId = "test",
+#            label = tags$a(href="https://trello.com/b/0u889JNF/ping",
+#                           img(src = "patte.png",
+#                               width = "30",
+#                               height = "30")))
+
+# output$hist <- renderPlot({
+#   hist(rnorm(input$n + 100*as.numeric(input$b)))
+# })
+
+
+
+
+
+
+
