c30dada22c02957b2e669fdcd55b09f028b1c2eb
FERRA <ferralaurianna@gmail.com>
2022-05-20 12:09:33 +0200

    Implémentation du changement de couleur des pictogrammes : il manque le changement de couleur des encadrés des chiffres globaux et la restriction des pictogrammes ronds en fonction des pictogrammes des pôles activés.

--- R/Server/createServer.R
+++ R/Server/createServer.R
@@ -160,5 +160,457 @@ server <- function(input, output, session) {
              ui = divBandeau)
   })
   
+  observeEvent(input$actionButtonFlower, {
+    print("Flower button pressed");
+    var<-as.numeric(input$actionButtonFlower)%%2+as.numeric(input$actionButtonBee)%%2+as.numeric(input$actionButtonPaw)%%2;
+    if(var==3||var==0)
+    {
+      removeUI(selector = '#actionButtonFlower>img')
+      insertUI(selector = '#actionButtonFlower',
+               ui = img(src = "Resources/pictogrammes/fleur_violet.png",
+                        width=50,
+                        height=50))
+      
+      removeUI(selector = '#actionButtonBee>img')
+      insertUI(selector = '#actionButtonBee',
+               ui = img(src = "Resources/pictogrammes/abeille_violet.png",
+                        width=50,
+                        height=50))
+      
+      removeUI(selector = '#actionButtonPaw>img')
+      insertUI(selector = '#actionButtonPaw',
+               ui = img(src = "Resources/pictogrammes/patte_violet.png",
+                        width=50,
+                        height=50))
+    }
+    else{
+      if(as.numeric(input$actionButtonFlower)%%2==1)
+      {
+        removeUI(selector = '#actionButtonFlower>img')
+        insertUI(selector = '#actionButtonFlower',
+                 ui = img(src = "Resources/pictogrammes/fleur.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonFlower>img')
+        insertUI(selector = '#actionButtonFlower',
+                 ui = img(src = "Resources/pictogrammes/fleur_gris.png",
+                          width=50,
+                          height=50))
+      }
+      
+      if(as.numeric(input$actionButtonBee)%%2==1)
+      {
+        removeUI(selector = '#actionButtonBee>img')
+        insertUI(selector = '#actionButtonBee',
+                 ui = img(src = "Resources/pictogrammes/abeille.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonBee>img')
+        insertUI(selector = '#actionButtonBee',
+                 ui = img(src = "Resources/pictogrammes/abeille_gris.png",
+                          width=50,
+                          height=50))
+      }
+      
+      if(as.numeric(input$actionButtonPaw)%%2==1)
+      {
+        removeUI(selector = '#actionButtonPaw>img')
+        insertUI(selector = '#actionButtonPaw',
+                 ui = img(src = "Resources/pictogrammes/patte.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonPaw>img')
+        insertUI(selector = '#actionButtonPaw',
+                 ui = img(src = "Resources/pictogrammes/patte_gris.png",
+                          width=50,
+                          height=50))
+      }
+        
+    }
+    print(as.numeric(input$actionButtonFlower));
+  })
   
+  observeEvent(input$actionButtonBee, {
+    print("Bee button pressed");
+    var<-as.numeric(input$actionButtonFlower)%%2+as.numeric(input$actionButtonBee)%%2+as.numeric(input$actionButtonPaw)%%2;
+    if(var==3||var==0)
+    {
+      removeUI(selector = '#actionButtonFlower>img')
+      insertUI(selector = '#actionButtonFlower',
+               ui = img(src = "Resources/pictogrammes/fleur_violet.png",
+                        width=50,
+                        height=50))
+      
+      removeUI(selector = '#actionButtonBee>img')
+      insertUI(selector = '#actionButtonBee',
+               ui = img(src = "Resources/pictogrammes/abeille_violet.png",
+                        width=50,
+                        height=50))
+      
+      removeUI(selector = '#actionButtonPaw>img')
+      insertUI(selector = '#actionButtonPaw',
+               ui = img(src = "Resources/pictogrammes/patte_violet.png",
+                        width=50,
+                        height=50))
+    }
+    else{
+      if(as.numeric(input$actionButtonFlower)%%2==1)
+      {
+        removeUI(selector = '#actionButtonFlower>img')
+        insertUI(selector = '#actionButtonFlower',
+                 ui = img(src = "Resources/pictogrammes/fleur.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonFlower>img')
+        insertUI(selector = '#actionButtonFlower',
+                 ui = img(src = "Resources/pictogrammes/fleur_gris.png",
+                          width=50,
+                          height=50))
+      }
+      
+      if(as.numeric(input$actionButtonBee)%%2==1)
+      {
+        removeUI(selector = '#actionButtonBee>img')
+        insertUI(selector = '#actionButtonBee',
+                 ui = img(src = "Resources/pictogrammes/abeille.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonBee>img')
+        insertUI(selector = '#actionButtonBee',
+                 ui = img(src = "Resources/pictogrammes/abeille_gris.png",
+                          width=50,
+                          height=50))
+      }
+      
+      if(as.numeric(input$actionButtonPaw)%%2==1)
+      {
+        removeUI(selector = '#actionButtonPaw>img')
+        insertUI(selector = '#actionButtonPaw',
+                 ui = img(src = "Resources/pictogrammes/patte.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonPaw>img')
+        insertUI(selector = '#actionButtonPaw',
+                 ui = img(src = "Resources/pictogrammes/patte_gris.png",
+                          width=50,
+                          height=50))
+      }
+      }
+    print(as.numeric(input$actionButtonBee));
+  })
+  
+  observeEvent(input$actionButtonPaw, {
+    print("Bee button pressed");
+    var<-as.numeric(input$actionButtonFlower)%%2+as.numeric(input$actionButtonBee)%%2+as.numeric(input$actionButtonPaw)%%2;
+    if(var==3||var==0)
+    {
+      removeUI(selector = '#actionButtonFlower>img')
+      insertUI(selector = '#actionButtonFlower',
+               ui = img(src = "Resources/pictogrammes/fleur_violet.png",
+                        width=50,
+                        height=50))
+      
+      removeUI(selector = '#actionButtonBee>img')
+      insertUI(selector = '#actionButtonBee',
+               ui = img(src = "Resources/pictogrammes/abeille_violet.png",
+                        width=50,
+                        height=50))
+      
+      removeUI(selector = '#actionButtonPaw>img')
+      insertUI(selector = '#actionButtonPaw',
+               ui = img(src = "Resources/pictogrammes/patte_violet.png",
+                        width=50,
+                        height=50))
+    }
+    else{
+      if(as.numeric(input$actionButtonFlower)%%2==1)
+      {
+        removeUI(selector = '#actionButtonFlower>img')
+        insertUI(selector = '#actionButtonFlower',
+                 ui = img(src = "Resources/pictogrammes/fleur.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonFlower>img')
+        insertUI(selector = '#actionButtonFlower',
+                 ui = img(src = "Resources/pictogrammes/fleur_gris.png",
+                          width=50,
+                          height=50))
+      }
+      
+      if(as.numeric(input$actionButtonBee)%%2==1)
+      {
+        removeUI(selector = '#actionButtonBee>img')
+        insertUI(selector = '#actionButtonBee',
+                 ui = img(src = "Resources/pictogrammes/abeille.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonBee>img')
+        insertUI(selector = '#actionButtonBee',
+                 ui = img(src = "Resources/pictogrammes/abeille_gris.png",
+                          width=50,
+                          height=50))
+      }
+      
+      if(as.numeric(input$actionButtonPaw)%%2==1)
+      {
+        removeUI(selector = '#actionButtonPaw>img')
+        insertUI(selector = '#actionButtonPaw',
+                 ui = img(src = "Resources/pictogrammes/patte.png",
+                          width=50,
+                          height=50))
+      }
+      else{
+        removeUI(selector = '#actionButtonPaw>img')
+        insertUI(selector = '#actionButtonPaw',
+                 ui = img(src = "Resources/pictogrammes/patte_gris.png",
+                          width=50,
+                          height=50))
+      }
+    }
+    print(as.numeric(input$actionButtonPaw));
+  })
+  
+  observeEvent(input$feuFlore, {
+    print("Feu flore pressed");
+    var<-as.numeric(input$feuFlore)%%2+as.numeric(input$feuInvertebre)%%2+as.numeric(input$feuVertebre)%%2;
+    if(var==3||var==0)
+    {
+      removeUI(selector = '#feuFlore>img')
+      insertUI(selector = '#feuFlore',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+      
+      removeUI(selector = '#feuInvertebre>img')
+      insertUI(selector = '#feuInvertebre',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+      
+      removeUI(selector = '#feuVertebre>img')
+      insertUI(selector = '#feuVertebre',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+    }
+    else{
+      if(as.numeric(input$feuFlore)%%2==1)
+      {
+        removeUI(selector = '#feuFlore>img')
+        insertUI(selector = '#feuFlore',
+                 ui = img(src = "Resources/pictogrammes/rond_vert.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuFlore>img')
+        insertUI(selector = '#feuFlore',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+      if(as.numeric(input$feuInvertebre)%%2==1)
+      {
+        removeUI(selector = '#feuInvertebre>img')
+        insertUI(selector = '#feuInvertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_orange.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuInvertebre>img')
+        insertUI(selector = '#feuInvertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+      if(as.numeric(input$feuVertebre)%%2==1)
+      {
+        removeUI(selector = '#feuVertebre>img')
+        insertUI(selector = '#feuVertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_bleu.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuVertebre>img')
+        insertUI(selector = '#feuVertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+    }
+    print(as.numeric(input$feuFlore));
+  })
+  
+  observeEvent(input$feuInvertebre, {
+    print("Feu invertebre pressed");
+    var<-as.numeric(input$feuFlore)%%2+as.numeric(input$feuInvertebre)%%2+as.numeric(input$feuVertebre)%%2;
+    if(var==3||var==0)
+    {
+      removeUI(selector = '#feuFlore>img')
+      insertUI(selector = '#feuFlore',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+      
+      removeUI(selector = '#feuInvertebre>img')
+      insertUI(selector = '#feuInvertebre',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+      
+      removeUI(selector = '#feuVertebre>img')
+      insertUI(selector = '#feuVertebre',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+    }
+    else{
+      if(as.numeric(input$feuFlore)%%2==1)
+      {
+        removeUI(selector = '#feuFlore>img')
+        insertUI(selector = '#feuFlore',
+                 ui = img(src = "Resources/pictogrammes/rond_vert.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuFlore>img')
+        insertUI(selector = '#feuFlore',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+      if(as.numeric(input$feuInvertebre)%%2==1)
+      {
+        removeUI(selector = '#feuInvertebre>img')
+        insertUI(selector = '#feuInvertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_orange.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuInvertebre>img')
+        insertUI(selector = '#feuInvertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+      if(as.numeric(input$feuVertebre)%%2==1)
+      {
+        removeUI(selector = '#feuVertebre>img')
+        insertUI(selector = '#feuVertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_bleu.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuVertebre>img')
+        insertUI(selector = '#feuVertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+    }
+    print(as.numeric(input$feuInvertebre));
+  })
+  
+  observeEvent(input$feuVertebre, {
+    print("Feu vertebre pressed");
+    var<-as.numeric(input$feuFlore)%%2+as.numeric(input$feuInvertebre)%%2+as.numeric(input$feuVertebre)%%2;
+    if(var==3||var==0)
+    {
+      removeUI(selector = '#feuFlore>img')
+      insertUI(selector = '#feuFlore',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+      
+      removeUI(selector = '#feuInvertebre>img')
+      insertUI(selector = '#feuInvertebre',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+      
+      removeUI(selector = '#feuVertebre>img')
+      insertUI(selector = '#feuVertebre',
+               ui = img(src = "Resources/pictogrammes/rond_violet.png",
+                        width=15,
+                        height=15))
+    }
+    else{
+      if(as.numeric(input$feuFlore)%%2==1)
+      {
+        removeUI(selector = '#feuFlore>img')
+        insertUI(selector = '#feuFlore',
+                 ui = img(src = "Resources/pictogrammes/rond_vert.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuFlore>img')
+        insertUI(selector = '#feuFlore',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+      if(as.numeric(input$feuInvertebre)%%2==1)
+      {
+        removeUI(selector = '#feuInvertebre>img')
+        insertUI(selector = '#feuInvertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_orange.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuInvertebre>img')
+        insertUI(selector = '#feuInvertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+      if(as.numeric(input$feuVertebre)%%2==1)
+      {
+        removeUI(selector = '#feuVertebre>img')
+        insertUI(selector = '#feuVertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_bleu.png",
+                          width=15,
+                          height=15))
+      }
+      else{
+        removeUI(selector = '#feuVertebre>img')
+        insertUI(selector = '#feuVertebre',
+                 ui = img(src = "Resources/pictogrammes/rond_gris.png",
+                          width=15,
+                          height=15))
+      }
+      
+    }
+    print(as.numeric(input$feuVertebre));
+  })
 }
